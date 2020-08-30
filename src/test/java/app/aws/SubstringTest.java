16eeacb2909c6826ceee3077fde3d0503e5fabf5
Sudarsana Kasireddy <sudaredd@gmail.com>
2020-08-30 12:38:30 -0400

    Read AWS Paramstore properties

---
+++ src/test/java/app/aws/SubstringTest.java
@@ -0,0 +1,12 @@
+package app.aws;
+
+import org.junit.jupiter.api.Test;
+
+public class SubstringTest {
+
+    @Test
+    public void testSubStr() {
+        String sre = "sre";
+        System.out.println(sre.substring(0, sre.length()-1));
+    }
+}
